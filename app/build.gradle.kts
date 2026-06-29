25866af9c6e4dff2db6f3c7f815923457189a56f
Tiziano Onor <tiziano@example.com>
2026-06-29 22:03:41 +0200

    ci: implement GitHub Actions for automated signed release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,10 +24,20 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
         }
     }
     compileOptions {
