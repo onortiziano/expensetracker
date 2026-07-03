a4b73805d8dba7eb2c0a3dbb37c14f2e06eb8f51
Tiziano Onor <tiziano@example.com>
2026-07-03 08:01:50 +0200

    Fix: bump database version to 3 to solve migration crash

--- app/src/main/java/it/ciano/expensetracker/data/AppDatabase.kt
+++ app/src/main/java/it/ciano/expensetracker/data/AppDatabase.kt
@@ -8,7 +8,7 @@ import it.ciano.expensetracker.data.dao.*
 import it.ciano.expensetracker.data.model.*
 
 // Definiamo le tabelle che compongono il database e la versione (1)
-@Database(entities = [Category::class, Transaction::class, Budget::class, Tag::class, TransactionTag::class], version = 2, exportSchema = false)
+@Database(entities = [Category::class, Transaction::class, Budget::class, Tag::class, TransactionTag::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun categoryDao(): CategoryDao
